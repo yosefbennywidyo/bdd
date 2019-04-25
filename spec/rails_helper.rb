2a030ad9398a9101152715d1eddbd72bdd4df1ca
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-25 10:30:20 +0800

    Database Cleaner gem: - add and install Database Cleaner gem - configurre cleaning strategy for RSpec test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,4 +70,18 @@ RSpec.configure do |config|
       with.library :rails
       end
     end
+
+  # Database Cleaner setup
+  # cleaning strategy for RSpec test
+  RSpec.configure do |config|
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
+  end
 end
