f90617d25743842e41b38aa623cbe3328170dd29
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-28 15:51:14 +0800

    Update bukan pengguna steps

--- features/step_definitions/bukan_pengguna_steps.rb
+++ features/step_definitions/bukan_pengguna_steps.rb
@@ -31,86 +31,61 @@ Maka("saya tidak dapat membuat Data Keagamaan Katolik") do
 	# fill_in "Keterangan", :with => "keterangan data keagamaan katolik yang dibuat oleh bukan pengguna"
 	# click_button "Membuat"
 	# save_and_open_page
-	expect(page).to have_content("Maaf, Anda tidak berhak membuatnya.")
+	expect(page).to have_content("Anda harus mendaftar atau sign in sebelum melanjutkan.")
 end
 
 Dan("saya tidak dapat membuat Data Pendidikan Agama Katolik") do
 	visit new_data_pendidikan_agama_katolik_path
-	fill_in "Judul", :with => "judul data pendidikan agama katolik yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan data pendidikan agama katolik yang dibuat oleh bukan pengguna"
-	click_button "Membuat"
 	expect(page).to have_content("Anda harus mendaftar atau sign in sebelum melanjutkan.")
 end
 
 Dan("saya tidak dapat membuat Informasi Berita Terkini") do
 	visit new_informasi_berita_terkini_path
-	fill_in "Judul", :with => "judul Informasi Berita Terkini yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan Informasi Berita Terkini yang dibuat oleh bukan pengguna"
-	click_button "Membuat"
-	expect(page).to have_content("Maaf, Anda tidak berhak membuatnya.")
+	expect(page).to have_content("Anda harus mendaftar atau sign in sebelum melanjutkan.")
 end
 
 Dan("saya tidak dapat membuat Informasi Pengumuman") do
 	visit new_informasi_pengumuman_path
-	fill_in "Judul", :with => "judul Informasi Pengumuman yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan Informasi Pengumuman yang dibuat oleh bukan pengguna"
-	click_button "Membuat"
-	expect(page).to have_content("Maaf, Anda tidak berhak membuatnya.")
+	expect(page).to have_content("Anda harus mendaftar atau sign in sebelum melanjutkan.")
 end
 
 Maka("saya tidak dapat mengedit Data Pendidikan Agama Katolik") do
 	visit edit_data_keagamaan_katolik_path
-	fill_in "Judul", :with => "judul data keagamaan katolik yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan data keagamaan katolik yang dibuat oleh bukan pengguna"
-	click_button "Edit"
 	expect(page).to have_content("Maaf, Anda tidak berhak mengeditnya.")
 end
 
 Dan("saya tidak dapat mengedit Data Keagamaan Katolik") do
 	visit edit_data_keagamaan_katolik_path
-	fill_in "Judul", :with => "judul data keagamaan katolik yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan data keagamaan katolik yang dibuat oleh bukan pengguna"
-	click_button "Edit"
 	expect(page).to have_content("Maaf, Anda tidak berhak mengeditnya.")
 end
 
 Dan("saya tidak dapat mengedit Informasi Berita Terkini") do
 	visit edit_informasi_berita_terkini_path
-	fill_in "Judul", :with => "judul Informasi Berita Terkini yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan Informasi Berita Terkini yang dibuat oleh bukan pengguna"
-	click_button "Edit"
 	expect(page).to have_content("Maaf, Anda tidak berhak mengeditnya.")
 end
 
 Dan("saya tidak dapat mengedit Informasi Pengumuman") do
 	visit edit_informasi_berita_terkini_path
-	fill_in "Judul", :with => "judul Informasi Pengumuman yang dibuat oleh bukan pengguna"
-	fill_in "Keterangan", :with => "keterangan Informasi Pengumuman yang dibuat oleh bukan pengguna"
-	click_button "Edit"
 	expect(page).to have_content("Maaf, Anda tidak berhak mengeditnya.")
 end
 
 Maka("saya tidak dapat menghapus Data Pendidikan Agama Katolik") do
 	visit data_keagamaan_katolik_path
-	click_button "Hapus"
 	expect(page).to have_content("Maaf, Anda tidak berhak menghapusnya.")
 end
 
 Dan("saya tidak dapat menghapus Data Keagamaan Katolik") do
 	visit data_keagamaan_katolik_path
-	click_button "Hapus"
 	expect(page).to have_content("Maaf, Anda tidak berhak menghapusnya.")
 end
 
 Dan("saya tidak dapat menghapus Informasi Berita Terkini") do
 	visit data_keagamaan_katolik_path
-	click_button "Hapus"
 	expect(page).to have_content("Maaf, Anda tidak berhak menghapusnya.")
 end
 
 Dan("saya tidak dapat menghapus Informasi Pengumuman") do
 	visit data_keagamaan_katolik_path
-	click_button "Hapus"
 	expect(page).to have_content("Maaf, Anda tidak berhak menghapusnya.")
 end
 
