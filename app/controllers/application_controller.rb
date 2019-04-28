632d828e3668b2743d7009db2eabf2dc437de560
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-29 02:34:50 +0800

    Devise setup: - change config.sign_out_via to get - change devise routes for Pengguna - redirect to root after signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@ class ApplicationController < ActionController::Base
 
 	protect_from_forgery with: :exception
 
+	protected
     # Never trust parameters from the scary internet, only allow the white list through.
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_in, keys: [:nama_lengkap, :email, :password, :remember_me])
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:nama_lengkap, :email, :password, :password_confirmation])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:nama_lengkap, :email, :password, :password_confirmation, :current_password])
+	end
+
+	# Overwriting the sign_out redirect path method
+	def sign_out_and_redirect(pengguna)
+		root_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Rails.application.routes.draw do
   resources :informasi_berita_terkini
   resources :data_pendidikan_agama_katolik
   resources :data_keagamaan_katolik
+
   devise_for :pengguna
+
+  as :pengguna do
+  	get 'signin', to: 'devise/sessions#new'
+  	post 'signin', to: 'devise/sessions#create'
+  	delete 'signout', to: 'devise/sessions#destroy'
+  end
+
 	root to: "beranda#index"
+
   get 'beranda/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
