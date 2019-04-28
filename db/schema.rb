2e802dd0b3f0eef1f1f93eecc551d1cbcc7fc1de
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-28 16:14:42 +0800

    Migrate nama_lengkap to Pengguna

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_072001) do
+ActiveRecord::Schema.define(version: 2019_04_28_080849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_072001) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.integer "peran"
+    t.string "nama_lengkap"
     t.index ["email"], name: "index_pengguna_on_email", unique: true
     t.index ["reset_password_token"], name: "index_pengguna_on_reset_password_token", unique: true
   end
