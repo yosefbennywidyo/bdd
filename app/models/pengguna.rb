4fdb0e5f34802008c4def3e3ac7407f4cf9e51d6
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-28 16:51:31 +0800

    Fix user validation

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -1,5 +1,5 @@
 class Pengguna < ApplicationRecord
-  validates :nama_lengkap, length: { minimum: 6 }, too_short: 'Nama lengkap terlalu pendek, minimal 6 karakter! Contoh: Rifaid'
+  validates_length_of :nama_lengkap, minimum: 6, too_short:  'Nama lengkap terlalu pendek, minimal 6 karakter! Contoh: Rifaid'
 	enum peran: [:pemirsa, :guru_pendakat, :pegawai_pendakat, :penyelenggara_pendakat, :penyuluh_urakat, :pegawai_urakat, :kasie_urakat, :administrator]
   after_initialize :set_peran_default, :if => :new_record?
   # Include default devise modules. Others available are:
