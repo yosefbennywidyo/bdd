95f050cdea1d335636d1e71b71388b45276bf717
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-05-01 17:49:02 +0800

    SendGrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# SendGrid Setup
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app132188869@heroku.com',
+  :password => 'vxmq0ypq6939',
+  :domain => 'app132188869@heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
