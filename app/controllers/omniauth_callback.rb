4088f2fb3e20cdbfc4516e2ceb0a21a21c8cce67
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-05-01 22:24:45 +0800

    Revert omniauth callback

--- app/controllers/omniauth_callback.rb
+++ app/controllers/omniauth_callback.rb
@@ -5,8 +5,7 @@ class Pengguna::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
 
       if @pengguna.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @pengguna
-        redirect_to root_path
+        sign_in_and_redirect @pengguna, event: :authentication
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
         redirect_to new_user_registration_url, alert: @pengguna.errors.full_messages.join("\n")
