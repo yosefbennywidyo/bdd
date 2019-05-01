b8d38eea69c8b44940de1c988c23845688de0a39
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-05-01 22:02:42 +0800

    Redirect to root_path

--- app/controllers/omniauth_callback.rb
+++ app/controllers/omniauth_callback.rb
@@ -6,7 +6,7 @@ class Pengguna::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
       if @pengguna.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @pengguna, event: :authentication
-        redirect_to :root
+        redirect_to root_path
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
         redirect_to new_user_registration_url, alert: @pengguna.errors.full_messages.join("\n")
