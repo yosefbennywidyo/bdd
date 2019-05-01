905d2f53761740af132faa4adef2524cd623a1df
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-05-01 21:52:06 +0800

    Migrate changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_134142) do
+ActiveRecord::Schema.define(version: 2019_05_01_134751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_134142) do
     t.string "nama_lengkap"
     t.string "google_token"
     t.string "google_refresh_token"
-    t.integer "sign_in_count"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
